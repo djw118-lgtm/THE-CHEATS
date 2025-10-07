f5627eeae9ee2d277ac69b611ee335f89bad89ec
djw118-lgtm <djw118-lgtm@users.noreply.github.com>
2025-10-07 23:30:33 +0000

    deploy: f13ff96379b44eec35dedd1652a25c1dc17b9630

--- index.js
+++ index.js
@@ -392,6 +392,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/THE-CHEATS/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
